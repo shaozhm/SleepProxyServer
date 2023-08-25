45fcbcb26adb478a342e2fc28995df4030c75e06
Shao, Zhimin <zhimin.shao@sap.com>
2023-08-25 23:43:21 +0800

    update

--- sleepproxy/wol.py
+++ sleepproxy/wol.py
@@ -1,10 +1,22 @@
 import logging
+import subprocess
 from scapy.all import sendp,  Ether, IP, UDP, Raw
 
 def wake(mac):
     logging.warning("Sending WOL packet to %s" % (mac, ))
     mac = mac.decode("hex")
-    sendp(Ether(dst='ff:ff:ff:ff:ff:ff') / IP(dst='255.255.255.255', flags="DF") / UDP(dport=9, sport=39227) / Raw('\xff' * 6 + mac * 16))
+    # sendp(Ether(dst='ff:ff:ff:ff:ff:ff') / IP(dst='255.255.255.255', flags="DF") / UDP(dport=9, sport=39227) / Raw('\xff' * 6 + mac * 16))
+    subprocess.run([
+    "/home/pi/blynk-library/scripts/blynk_ctrl.py", 
+    "-s",
+    "sonospi.local",
+    "-p",
+    "8442",
+    "-t",
+    "BO9Ej28AzpoEsaCs0WXiS3mqSO2KE8mZ",
+    "-vw",
+    "73",
+    "1"], stdout=subprocess.DEVNULL)
 
 if __name__ == '__main__':
     import sys
